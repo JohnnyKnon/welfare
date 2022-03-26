b9074114bbed5352a4ef5745afbee2ec0d5fbfaa
Johnny hong <86140440+JohnnyKnon@users.noreply.github.com>
2022-03-26 09:56:35 +0900

    Scroll Up part
    JS를 이용해서 scrollY값이 200 보다
    클때 등장하도록 설정

--- child/js/default/scrollUpBtn.js
+++ child/js/default/scrollUpBtn.js
@@ -4,3 +4,19 @@ function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
 }
+
+const SCROLL_UP_BTN = document.querySelector('.scrollUp');
+
+SCROLL_UP_BTN.addEventListener('click', () => {
+  scrollIntoView('#total__header');
+});
+
+// scroll event
+window.addEventListener('scroll', () => {
+  let scrollValue = window.scrollY;
+  if (scrollValue > 200) {
+    SCROLL_UP_BTN.classList.add('scrollUp-active');
+  } else if (scrollValue < 200) {
+    SCROLL_UP_BTN.classList.remove('scrollUp-active');
+  }
+});

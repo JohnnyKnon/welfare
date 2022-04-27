9b950ca63a46cf439554a1c1fc172bdb4f069719
Johnny hong <86140440+JohnnyKnon@users.noreply.github.com>
2022-04-27 14:00:48 +0900

    sub06 오시는 길 완료 / sub01 인사말 시작

--- child/js/default/scrollUpBtn.js
+++ child/js/default/scrollUpBtn.js
@@ -1,14 +1,8 @@
 'use strict';
-// Scroll Up btn
-function scrollIntoView(selector) {
-  const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({ behavior: 'smooth' });
-}
-
 const SCROLL_UP_BTN = document.querySelector('.scrollUp');
 
 SCROLL_UP_BTN.addEventListener('click', () => {
-  scrollIntoView('#total__header');
+  scrollIntoView('.top');
 });
 
 // scroll event
@@ -20,3 +14,9 @@ window.addEventListener('scroll', () => {
     SCROLL_UP_BTN.classList.remove('scrollUp-active');
   }
 });
+
+// Scroll Up btn
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
